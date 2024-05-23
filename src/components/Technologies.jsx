ba58ab8e6ca5808a06c5d60b4150846f708e0b61
Leandro <ljborges76@gmail.com>
2024-05-23 00:22:14 -0300

    feat: creates Technologies component

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,36 @@
+import { RiReactjsLine } from "react-icons/ri"
+import { FaAngular } from "react-icons/fa"
+import { FaPython } from "react-icons/fa"
+import { FaJava } from "react-icons/fa"
+import { FaPhp } from "react-icons/fa"
+import { DiMysql } from "react-icons/di"
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+        <div className="flex flex-wrap items-center justify-center gap-4">
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400"/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaAngular className="text-7xl text-red-600"/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaPython className="text-7xl text-yellow-400"/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaJava className="text-7xl text-blue-800"/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaPhp className="text-7xl text-sky-900"/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiMysql className="text-7xl text-sky-800"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Technologies
